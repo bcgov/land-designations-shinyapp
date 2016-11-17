944bf005f01fbf8055d47fd763bcb2a4e252edf5
Andy Teucher <andy.teucher@gov.bc.ca>
2016-11-17 13:28:02 -0800

    Add dev-win.sh

--- dev-win.sh
+++ dev-win.sh
@@ -26,18 +26,9 @@ docker build -t shinylands -f Dockerfile.local .
 
 # --------------------------------------------------------
 #
-# this runs the image and mounts everything interesting
-# to your local repo so you can play and see the logs
-# without having to go into the container (hopefully)
+# Run the image
 #
 # --------------------------------------------------------
-docker run --rm --name shiny \
-	-p 3838:3838 \
-	-v `pwd`/_mount/bookmarks:/var/lib/shiny-server \
-	-v `pwd`/_mount/logs:/var/log/shiny-server \
-	-v `pwd`/_mount/output:/srv/shiny-server-output \
-	-v `pwd`/_mount/tmp:/tmp \
-	-v `pwd`/app:/srv/shiny-server \
-	shinylands
+docker run -i -t --rm --name shinylands -p 3838:3838 shinylands
 
 
