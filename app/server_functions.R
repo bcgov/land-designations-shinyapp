757ed143e1f92f651a91f9e539acd93a99e7dc3c
Andy Teucher <andy.teucher@gov.bc.ca>
2017-01-23 16:22:04 -0800

    Remove y gridlines

--- app/server_functions.R
+++ app/server_functions.R
@@ -56,7 +56,9 @@ ggiraph_barchart <- function(df, type) {
                          aes(fill = reverse_factor(category), tooltip = hovertip, data_id = hovertip))) +
     scale_fill_manual(values = des_cols) +
     theme_minimal(base_size = 15) +
-    theme(axis.title.x = element_text(hjust = 1)) +
+    theme(axis.title.x = element_text(hjust = 1),
+          panel.grid.major.y = element_blank(),
+          panel.grid.minor.y = element_blank()) +
     scale_y_continuous(expand = c(0,0)) +
     scale_x_discrete(expand = c(0,0), labels = prot_rollup_labels) +
     coord_flip() +
